7acb8fe82d1e6c343a0f7aa9792286f0285f0481
Prasun Roy <prasunroy212@gmail.com>
2020-08-15 17:13:59 +0530

    Adding new features

--- Desktop Assistant.py
+++ Desktop Assistant.py
@@ -1,14 +1,14 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import pyttsx3  # pip install pyttsx3
+import speech_recognition as sr  # pip install speechRecognition
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia  # pip install wikipedia
 import webbrowser
 import os
 import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
+# print(voices[0].id)
 engine.setProperty('voice', voices[1].id)
 
 
@@ -19,36 +19,42 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning! ")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 17:
         speak("Good Afternoon! ")
 
-    else:
+    elif hour >= 17 and hour < 19:
         speak("Good Evening! ")
+
+    else:
+        speak("Good Night! ")
 
     speak("I am your  Vertual  Assistant  Sir.  Made by prasun roy. Please tell me how may I help you")
 
-def takeCommand():
-    #It takes microphone input from the user and returns string output
 
-    r = sr.Recognizer()
+def takeCommand():
+    # It takes microphone input from the user and returns string output
+
+    rr = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
+        rr.pause_threshold = 1
+        audio = rr.listen(source)
 
     try:
         print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
+        query = rr.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
 
     except Exception as e:
         # print(e)
         print("Say that again please...")
+        speak("Connection error")
         return "None"
     return query
+
 
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -58,10 +64,11 @@ def sendEmail(to, content):
     server.sendmail('youremail@gmail.com', to, content)
     server.close()
 
+
 if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
+        # if 1:
         query = takeCommand().lower()
 
         # Logic for executing tasks based on query
@@ -73,38 +80,110 @@ if __name__ == "__main__":
             print(results)
             speak(results)
 
-        elif 'hai' in query:
-            speak("Hello!")
+        elif "hello" in query or "hello Jarvis" in query:
+            hello1 = "Hello Sir ! How May i Help you.."
+            print(hello1)
+            speak(hello1)
 
-        elif 'Who are you' in query :
-            speak("I am your vertual assistant.")
+        elif "who are you" in query or "about you" in query or "your details" in query:
+            who_are_you = "I am Jarvis an A I based computer program but i can help you lot like a your assistant ! try me to give simple command !"
+            print(who_are_you)
+            speak(who_are_you)
 
-        elif 'who made you' in query:
-            speak(" Prasun Roy")
+        elif 'make you' in query or 'created you' in query or 'develop you' in query:
+            speak(" For your information Prasun Roy Created me !")
 
         elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            webbrowser.open("www.youtube.com")
+            speak("opening youtube")
+
+        elif 'open github' in query:
+            webbrowser.open("https://www.github.com")
+            speak("opening github")
+
+        elif 'open facebook' in query:
+            webbrowser.open("https://www.facebook.com")
+            speak("opening facebook")
+
+        elif 'open instagram' in query:
+            webbrowser.open("https://www.instagram.com")
+            speak("opening instagram")
 
         elif 'open google' in query:
             webbrowser.open("google.com")
+            speak("opening google")
 
         elif 'open stackoverflow' in query:
             webbrowser.open("stackoverflow.com")
+            speak("opening stackoverflow")
 
+        elif 'open yahoo' in query:
+            webbrowser.open("https://www.yahoo.com")
+            speak("opening yahoo")
+
+        elif 'open gmail' in query:
+            webbrowser.open("https://mail.google.com")
+            speak("opening google mail")
+
+        elif 'open snapdeal' in query:
+            webbrowser.open("https://www.snapdeal.com")
+            speak("opening snapdeal")
+
+        elif 'open amazon' in query or 'shop online' in query:
+            webbrowser.open("https://www.amazon.com")
+            speak("opening amazon")
+
+        elif 'open flipkart' in query:
+            webbrowser.open("https://www.flipkart.com")
+            speak("opening flipkart")
 
         elif 'play music' in query:
-            music_dir = 'E:\\My MUSIC\\HARE_HARE_RAMA.mp3'
+            speak("ok i am playing music")
+            music_dir = 'E:\\My MUSIC'
             songs = os.listdir(music_dir)
             print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'video from pc' in query or "video" in query:
+            speak("ok i am playing videos")
+            video_dir = 'E:\\\My Videos'
+            Videos = os.listdir(video_dir)
+            print(Videos)
+            os.startfile(os.path.join(video_dir, videos[0]))
+
+        elif 'good bye' in query:
+            speak("good bye")
+            exit()
+
+        elif "shutdown" in query:
+            speak("shutting down")
+            os.system('shutdown -s')
+
+        elif "your name" in query or "sweat name" in query:
+            naa_mme = "Thanks for Asking my self ! Jarvis"
+            print(naa_mme)
+            speak(naa_mme)
+
+        elif "you feeling" in query:
+            print("feeling Very happy to help you")
+            speak("feeling Very happy to help you")
+
+        elif query == 'none':
+            continue
+
+        elif 'exit' in query or 'stop' in query or 'quit' in query:
+            exx_exit = 'See you soon. Bye'
+            speak(exx_exit)
+            exit()
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "D:\\vs\\Microsoft VS Code\\code.exe"
+            codePath = "D:\\vs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
+            speak("opening visual studio code")
 
         elif 'email to prasun' in query:
             try:
@@ -116,3 +195,21 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 speak("Sorry Prasun Sir. I am not able to send this email")
+
+        elif 'how are you' in query:
+            setMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'i am fine ! How are you']
+            ans_qus = random.choice(setMsgs)
+            speak(ans_qus)
+            ans_from_user_how_are_you = takeCommand()
+            if 'fine' in ans_from_user_how_are_you or 'happy' in ans_from_user_how_are_you or 'okey' in ans_from_user_how_are_you:
+                speak('Great')
+            elif 'not' in ans_from_user_how_are_you or 'sad' in ans_from_user_how_are_you or 'upset' in ans_from_user_how_are_you:
+                speak('Tell me how can i make you happy')
+
+        else:
+            tempp = query.replace(' ', '+')
+            prasun_url = "https://www.google.com/search?q="
+            res_prasun = 'sorry! i cant understand but i search from internet to give your answer !'
+            print(res_prasun)
+            speak(res_prasun)
+            webbrowser.open(prasun_url + tempp)
